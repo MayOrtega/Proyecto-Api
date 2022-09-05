e1a7a18c9971c1d63ddb2c34e8a32874a140b550
MayorlenO <mayorlen.ortega@gmail.com>
2022-09-05 15:07:41 -0300

    app recibirá la información

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 //Importando las librerías instaladas
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const dbConnect = require("./config/mongo")
+const dbConnect = require("./config/mongo");
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
+//Debemos preparar al app para recibir info a través de un Post
+app.use(express.json());
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 //Aquí invocamos a la rutas
 
-app.use("/api", require("./routes"))
-
-
+app.use("/api", require("./routes"));
 
 app.listen(port, () => {
-    console.log("El puerto es: " + port)
+  console.log("El puerto es: " + port);
 });
 
-dbConnect()
+dbConnect();
